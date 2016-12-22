ff8ab47b2b796f15d6e685fc31558bcb986a018d
tomvde <tom.vandeneynde@gmail.com>
2016-12-22 09:30:24 +0100

    functions

---
+++ java8/src/be/tvde/lambdas/java8_lamba_toolbox/FunctionOverTime.java
@@ -0,0 +1,34 @@
+package be.tvde.lambdas.java8_lamba_toolbox;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Created by tomvde on 21/12/2016.
+ */
+@FunctionalInterface
+public interface FunctionOverTime {
+    double valueAt(int time);
+
+    static FunctionOverTime monthByMonth(final double[] values) {
+        return time -> values[time - 1];
+    }
+
+    static FunctionOverTime constant(final double value) {
+        return polynomial(new double[] { value });
+    }
+
+    static FunctionOverTime line(final double intercept, final double slope) {
+        AtomicReference<String> as;
+        return polynomial(new double[] { intercept, slope });
+    }
+
+    static FunctionOverTime polynomial(final double[] coefficients) {
+        return time -> {
+            Double sum = 0.0;
+            for (int i = 0; i < coefficients.length; i++) {
+                sum += Math.pow(time, i) * coefficients[i];
+            }
+            return sum;
+        };
+    }
+}
